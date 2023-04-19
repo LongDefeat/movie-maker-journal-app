6c709bede0c46775bb81b01f4e17d92abb0d141d
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-04-18 21:18:05 -0700

    add movie genre css

--- src/movies/MovieGenres.js
+++ src/movies/MovieGenres.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../auth/UserContext";
 import MovieDatabaseApi from "../api/MovieDatabaseApi";
+import "./MovieGenres.css";
 
 function MovieGenres() {
   const currentUser = useContext(UserContext);
   const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   useEffect(() => {
     async function fetchGenres() {
@@ -14,18 +16,28 @@ function MovieGenres() {
     fetchGenres();
   }, []);
 
+  function handleGenreClick(genreId) {
+    setSelectedGenre(genreId);
+  }
   return (
     <>
       <div>
         <h1>Genres</h1>
-        {genres && (
-          <ul>
-            {genres.map((genre) => (
-              <li key={genre.id}>{genre.name}</li>
-            ))}
-          </ul>
-        )}
       </div>
+      <ul className="genre-list">
+        {genres &&
+          genres.map((genre) => (
+            <li
+              key={genre.id}
+              className={`genre-item ${
+                genre.id === selectedGenre ? "selected-genre" : ""
+              }`}
+              onClick={() => handleGenreClick(genre.id)}
+            >
+              {genre.name}
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
