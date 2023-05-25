a41ac45537bbd40a9dc52f77d1fe8eb07a611b1c
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-05-24 20:59:52 -0700

    add navigate to homepage upon signup

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -1,106 +1,108 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Row, Col, Form, Button} from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
+function SignupForm({ signup }) {
+  const navigate = useNavigate();
+  const [signupFormData, setSignupFormData] = useState({
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
-
-function SignupForm ({signup}) {
-    const navigate = useNavigate();
-    const [signupFormData, setSignupFormData] = useState({
-        username: "",
-        password: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-    });
-
-    /** Handle form submission
-     * 
-     * Calls login function prop and, if successful, redirects back to / homepage
-     */
-
-    async function handleSubmit(e){
-        e.preventDefault();
-        let res = await signup(signupFormData);
-        if (res.success){
-           navigate("/")
-        } else {
-            console.log(res.errors);
-        }
+  /** Handle form submission
+   *
+   * Calls login function prop and, if successful, redirects back to / homepage
+   */
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let res = await signup(signupFormData);
+    if (res.success) {
+      navigate("/");
+    } else {
+      console.log(res.errors);
     }
+  }
 
-    /** Update form data field */
-    function handleChange(e) {
-        const {name, value} = e.target;
-        setSignupFormData(data => ({...data, [name]: value}));
-    }
+  /** Update form data field */
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setSignupFormData((data) => ({ ...data, [name]: value }));
+  }
 
-    return (
-        <div className="py-5">
-            <Row className="jusify-content-center">
-                <Col lg={12}>
-                    <Form className="form-signup">
-                        <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-                            <h2 className="mb-3">Sign Up</h2>
-                            <p className="welcome-message">Meet other movie makers and memorialize your movie experiences.</p>
+  return (
+    <div className="py-5">
+      <Row className="justify-content-center">
+        <Col lg={12}>
+          <Form className="form-signup">
+            <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+              <h2 className="mb-3">Sign Up</h2>
+              <p className="welcome-message">
+                Meet other movie makers and memorialize your movie experiences.
+              </p>
 
-                            <Form.Group className="mb-3" controlId="formBasicUsername">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control placeholder="Username"
-                                            type="text"
-                                            name="username"
-                                            className="form-control"
-                                            value={signupFormData.username}
-                                            onChange={handleChange}
-                                />
-                                
-                            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                  className="form-control"
+                  value={signupFormData.username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" 
-                                            placeholder="Password"
-                                            name="password"
-                                            className="form-control"
-                                            value={signupFormData.password}
-                                            onChange={handleChange} 
-                                />
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="form-control"
+                  value={signupFormData.password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
-                            </Form.Group>
-                    
-                            <Form.Group className="mb-3">
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control placeholder="Enter First Name"
-                                            name="firstName"
-                                            type="text"
-                                            className="form-control"
-                                            value={signupFormData.firstName}
-                                            onChange={handleChange}
-                                />
-                                
-                            </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  placeholder="Enter First Name"
+                  name="firstName"
+                  type="text"
+                  className="form-control"
+                  value={signupFormData.firstName}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
-                            <Form.Group className="mb-3">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control placeholder="Enter Last Name" 
-                                            name="lastName"
-                                            className="form-control"
-                                            value={signupFormData.lastName}
-                                            onChange={handleChange}
-                                />
-                                
-                            </Form.Group>
-                        
-                            <Button variant="outline-success" type="submit" onClick={handleSubmit}>
-                                Submit
-                            </Button>
-                        </div>
-                    </Form>
-                </Col>
-            </Row>
-        </div>
-    )
+              <Form.Group className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  placeholder="Enter Last Name"
+                  name="lastName"
+                  className="form-control"
+                  value={signupFormData.lastName}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
+              <Button
+                variant="outline-success"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default SignupForm;
--- src/movies/MovieCardList.js
+++ src/movies/MovieCardList.js
@@ -3,35 +3,30 @@ import MovieCard from "./MovieCard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
-/** Display list of movies
- * 
- */
-
-function MovieCardList ({movies}){
-
-    return (
-        <div>
-            <Row>
-               {movies.length ?  movies.map(movie => (
-                    
-                    <Col xs={12} md={6} lg={2} key={movie.id}>
-                        <MovieCard
-                            key={movie.id}
-                            id={movie.id}
-                            title={movie.title}
-                            poster={movie.poster_path}
-                            overview={movie.overview}
-                            releaseDate={movie.release_date}
-                            voteAverage={movie.vote_average}
-                        />
-                    </Col>
-                )) : <p>Sorry, no results were found...</p>}
-             
-            </Row>
-               
-        </div>
-    )
+function MovieCardList({ movies }) {
+  return (
+    <div>
+      <Row xs={1} sm={2} md={3} lg={4} xl={6} xxl={8} className="g-4">
+        {movies.length ? (
+          movies.map((movie) => (
+            <Col key={movie.id}>
+              <MovieCard
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                poster={movie.poster_path}
+                overview={movie.overview}
+                releaseDate={movie.release_date}
+                voteAverage={movie.vote_average}
+              />
+            </Col>
+          ))
+        ) : (
+          <p>Sorry, no results were found...</p>
+        )}
+      </Row>
+    </div>
+  );
 }
 
 export default MovieCardList;
