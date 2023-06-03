7a8a7472ca6ea376062e961aafb0a76214ceb715
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-06-03 10:38:13 -0700

    add heroku link

--- src/api/UserDatabaseApi.js
+++ src/api/UserDatabaseApi.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://movie-maker-journal-api.herokuapp.com/"
+    : "http://localhost:3001";
 class UserDatabaseApi {
   // token for interactions with the User Database
   static token;
